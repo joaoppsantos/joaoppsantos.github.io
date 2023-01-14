df5499a52b72712f0e92dce4ca5d70e22fc38b91
joaoppsantos <joaoppsantos@live.com>
2023-01-14 14:47:56 +0000

    feat: Remove Projects from Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,6 @@ function Navbar() {
         </div>
         <div className='links'>
             <Link to="/">Home</Link>
-            <Link to="/projects">Projects</Link>
             <Link to="/experience">Experience</Link>
         </div>
     </div>
