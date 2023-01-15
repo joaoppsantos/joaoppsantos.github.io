4e7bcaea89b7da43829fc78315ccd26790b1cc5d
joaoppsantos <joaoppsantos@live.com>
2023-01-15 18:46:26 +0000

    feat: Change flag-pt position and run prettier in Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,64 +1,94 @@
-import { LinkedIn, Email, GitHub } from '@material-ui/icons'
-import { Link } from "react-router-dom"
+import { LinkedIn, Email, GitHub } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 import "/node_modules/flag-icons/css/flag-icons.min.css";
-import { Tooltip } from '@material-ui/core'
-import { useTypewriter } from 'react-simple-typewriter'
-import React from 'react'
-import "../styles/Home.css"
+import { Tooltip } from "@material-ui/core";
+import { useTypewriter } from "react-simple-typewriter";
+import React from "react";
+import "../styles/Home.css";
 
 function Home() {
   const [typewriteText] = useTypewriter({
     words: ["Hey, I'm João!"],
-    loop: 1
-  })
+    loop: 1,
+  });
 
   return (
-    <div className='home'>
-      <div className='about'>
-        <div className='prompt'>
-          <h2>{typewriteText}</h2> 
-          <p className='description greetings'>
-            My name is João Santos, I'm a front-end developer based in Porto, Portugal.
+    <div className="home">
+      <div className="about">
+        <div className="prompt">
+          <h2>{typewriteText}</h2>
+          <p className="description greetings">
+            My name is João Santos, I'm a front-end developer based in Porto,
+            Portugal <span class="fi fi-pt flag"></span>
           </p>
-          <p className='description details'>
-            I have more than 6 years of experience in the area and I'm passionate in creating simple
-            solutions to complex problems. Feel free to go to <Link to="/experience" className='experienceLink'>Experience tab</Link> to check on my past experiences. 
-            <span class="fi fi-pt flag"></span> 
+          <p className="description details">
+            I have more than 6 years of experience in the area and I'm
+            passionate in creating simple solutions to complex problems. Feel
+            free to go to{" "}
+            <Link to="/experience" className="experienceLink">
+              Experience tab
+            </Link>{" "}
+            to check on my past experiences.
           </p>
-          <div className='tooltips'>
-            <Tooltip title={<h3 style={{ color: "white" }}>https://www.linkedin.com/in/joaopedropsantos/</h3>}>
-              <a href='https://www.linkedin.com/in/joaopedropsantos/'>
+          <div className="tooltips">
+            <Tooltip
+              title={
+                <h3 style={{ color: "white" }}>
+                  https://www.linkedin.com/in/joaopedropsantos/
+                </h3>
+              }
+            >
+              <a href="https://www.linkedin.com/in/joaopedropsantos/">
                 <LinkedIn />
               </a>
             </Tooltip>
-            <Tooltip title={<h3 style={{ color: "white" }}>mailto: joaoppsantos@live.com</h3>}>
-              <a href='mailto:joaoppsantos@live.com'>
+            <Tooltip
+              title={
+                <h3 style={{ color: "white" }}>
+                  mailto: joaoppsantos@live.com
+                </h3>
+              }
+            >
+              <a href="mailto:joaoppsantos@live.com">
                 <Email />
               </a>
             </Tooltip>
-            <Tooltip title={<h3 style={{ color: "white" }}>https://github.com/joaoppsantos</h3>}>
-              <a href='https://github.com/joaoppsantos'>
+            <Tooltip
+              title={
+                <h3 style={{ color: "white" }}>
+                  https://github.com/joaoppsantos
+                </h3>
+              }
+            >
+              <a href="https://github.com/joaoppsantos">
                 <GitHub />
               </a>
             </Tooltip>
           </div>
         </div>
       </div>
-      <div className='skills'>
+      <div className="skills">
         <h1> Skills</h1>
-        <ol className='list'>
-          <li className='item'>
+        <ol className="list">
+          <li className="item">
             <h2> Front-End</h2>
-            <span> Javascript, ReactJS, Typescript, Redux, HTML, CSS, MaterialUI, React Context API, Hooks, Apollo GraphQL, React Testing Library, Enzyme, Jest, Node.JS </span>
+            <span>
+              Javascript, ReactJS, Typescript, Redux, HTML, CSS, MaterialUI,
+              React Context API, Hooks, Apollo GraphQL, React Testing Library,
+              Enzyme, Jest, Node.JS
+            </span>
           </li>
-          <li className='item'>
+          <li className="item">
             <h2> Tooling and General Knowledge</h2>
-            <span> NPM, Yarn, Webpack, Jenkins, CircleCI, Scrum, Kanban, Git, Gitlab, Selenium, Webdriver.IO </span>
+            <span>
+              NPM, Yarn, Webpack, Jenkins, CircleCI, Scrum, Kanban, Git, Gitlab,
+              Selenium, Webdriver.IO
+            </span>
           </li>
         </ol>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
